2b042075eb38bc8d3b67194beca0fef8b9a0b855
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-08 22:13:57 +0900

    todoリストの作成

--- src/App.js
+++ src/App.js
@@ -2,21 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import {useRecoilState} from "recoil";
 import {counterAtom} from "./store/atom";
+import {useState} from "react";
 
+let maxId = 0;
 function App() {
 
-    const [counter, setCounter] = useRecoilState(counterAtom);
+    const [title, setTitle] = useState('');
+    const [todo, setTodo] = useState([]);
+
+    const handleChangeTitle = e => {
+        setTitle(e.target.value);
+    };
 
     const handleClick = () => {
-      setCounter(counter + 1);
-    }
+        setTodo([...todo,{
+            id: ++maxId,
+            title,
+            created: new Date(),
+            isDone: false
+        }
+        ]);
+    };
 
-  return (
-    <>
-      <button onClick={handleClick}>カウント</button>
-      <p>{counter}回、クリックされました。</p>
-    </>
-  );
+    console.log(title);
+    console.log(todo);
+
+    return (
+        <div>
+            <label>
+                やること：
+                <input type="text" name="title" value={title} onChange={handleChangeTitle}/>
+            </label>
+            <button type="button" onClick={handleClick}>追加</button>
+            <hr />
+            <ul>
+                {todo.map(item => (
+                        <li key={item.id}>{item.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
